3692cc8aa7dc352e8bd33735dec90ac997e9b0d1
墨七 <meichangliang@mo7.cc>
2022-05-30 00:41:49 +0800

    exit-push

--- router/Index.go
+++ router/Index.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"WebHook.net/config/public"
+	"WebHook.net/global"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,7 @@ func Index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index/index.tmpl", gin.H{
 		"title":     "WebHook.net",
 		"ShellList": public.ShellFile,
+		"Ping":      global.AppPackage.Version,
 	})
 }
 
