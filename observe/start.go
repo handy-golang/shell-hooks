5ed0422cf114632371bd69995f039b64c8a38506
墨七 <meichangliang@mo7.cc>
2022-05-30 14:40:59 +0800

    exit-push

--- observe/start.go
+++ observe/start.go
@@ -2,14 +2,25 @@ package observe
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
 
 	"WebHook.net/config/public"
 	"WebHook.net/global"
+	"github.com/EasyGolang/goTools/mCycle"
 	"github.com/EasyGolang/goTools/mPath"
 )
 
 func Start() {
+	mCycle.New(mCycle.Opt{
+		Func:      SetShell,
+		SleepTime: time.Minute * 2,
+	}).Start()
+}
+
+func SetShell() {
 	// 在这里读取文件列表,并打印 shell 文件
 	isShellPath := mPath.Exists(global.UserEnv.ShellPath)
 
@@ -19,22 +30,25 @@ func Start() {
 		panic(errStr)
 	}
 
-	fileInfoList, err := ioutil.ReadDir(global.UserEnv.ShellPath)
+	var files []string
+	root := global.UserEnv.ShellPath
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
 	if err != nil {
-		errorsStr := fmt.Errorf("目录读取失败")
-		panic(errorsStr)
+		panic(err)
 	}
 
-	for i := range fileInfoList {
-		name := fileInfoList[i].Name()
-		path := global.UserEnv.ShellPath + "/" + name
-
-		if mPath.IsFile(path) {
+	for key, file := range files {
+		if mPath.IsFile(file) {
 			SObj := public.ShellType{
-				ID:   i,
-				Name: name,
-				Path: path,
+				ID:   key,
+				Name: path.Base(file),
+				Path: file,
 			}
+
 			public.ShellFile = append(public.ShellFile, SObj)
 		}
 	}
