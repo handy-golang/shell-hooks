190d42d86cbc823fdf7a03efa1dad01488a62d41
墨七 <mo7@mo7.cc>
2023-05-04 14:06:09 +0800

    x

--- server/tmpl/index.go
+++ server/tmpl/index.go
@@ -4,16 +4,5 @@ import (
 	"embed"
 )
 
-//go:embed email.html
-var Email string
-
-type EmailParam struct {
-	Message string
-	SysTime string
-}
-
 //go:embed *
 var Static embed.FS
-
-//go:embed index/index.html
-var IndexHtml []byte
